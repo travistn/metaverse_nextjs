b6589448df0fdbfb469f8b438ba52a9d2ac6bcfb
travistn <travisn714@gmail.com>
2023-02-23 13:52:59 -0800

    Created TitleText component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -9,9 +9,19 @@ export const TypingText = ({ title, textStyles }) => (
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
     {Array.from(title).map((letter, index) => (
-      <motion.span variants={textVariant2}>{letter === ' ' ? '\u00A0' : letter}</motion.span>
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
     ))}
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial='hidden'
+    whileInView='show'
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}>
+    {title}
+  </motion.h2>
+);
